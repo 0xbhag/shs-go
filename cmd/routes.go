b771ad846d971de973b5977bfc6deb8b67c16bdb
0xbhag <charAnais@proton.me>
2026-09-24 14:19:02 +0330

    fix: embed static files for universal binary use

--- cmd/routes.go
+++ cmd/routes.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"shs/static"
 )
 
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("static"))
-	fmt.Printf("%+v", fileServer)
+	fileServer := http.FileServerFS(static.Files)
+	// fmt.Printf("%+v", fileServer)
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("GET /{$}", app.fileBrowserHandler)
 	mux.HandleFunc("GET /download/{filename}", app.fileDownloadHandler)
